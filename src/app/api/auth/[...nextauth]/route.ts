854403441c7e9145ce776ceb66c3b0086c9a15d6
Maaasyn <55663602+Maaasyn@users.noreply.github.com>
2023-04-24 09:01:24 +0200

    feat: added non conflicting port (#8)
    Co-authored-by: marcin@wajdzik.com <marcin@wajdzik.com>

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
 // const handler = NextAuth({});
 
 // export { handler as GET, handler as POST };
+export async function GET(request: Request) {
+  return new Response("TODO: auth");
+}
